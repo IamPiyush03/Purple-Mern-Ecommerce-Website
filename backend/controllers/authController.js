54d7410dd452be010dc420518765300e44ce5d80
IamPiyush03 <piyushdhondge03@gmail.com>
2024-11-26 17:11:38 +0530

    deleting errors

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 const User = require("../models/User"); // Assuming User model exists and is set up to interact with your DB
 const Admin=require("../models/Admin")
 
